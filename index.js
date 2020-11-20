3d176000870da603380161a4103035c9979d80d6
meeree <j.a.hazelden@gmail.com>
2020-11-20 14:14:09 -0700

    First prototype!

--- index.js
+++ index.js
@@ -1,8 +1,11 @@
+var cors = require('cors')
+
 const express = require('express');
 const bodyParser = require('body-parser');
 const keys = require('./config/keys');
 
 const app = express();
+app.use(cors())
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
